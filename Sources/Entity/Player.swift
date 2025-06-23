bf77626226d756340d0dc04ee084e0ba67ef36d4
RaissaParente <raissabparente@gmail.com>
2025-06-23 17:07:03 -0300

    fix: public inits

--- Sources/Entity/Player.swift
+++ Sources/Entity/Player.swift
@@ -5,7 +5,7 @@ public final class Player {
   public  let name: String
   public  var points: Int
     
-    init(name: String, id: UUID = UUID(), points: Int = 0) {
+    public init(name: String, id: UUID = UUID(), points: Int = 0) {
         self.name = name
         self.points = points
     }
--- Sources/Entity/Response.swift
+++ Sources/Entity/Response.swift
@@ -6,7 +6,7 @@ public final class Response: Equatable{
     public var isRepeat: Bool
     public var isCorrect: Bool
     public var isBest: Bool
-    init(text: String, isRepeat: Bool = false, isCorrect: Bool = true, isBest: Bool = false) {
+    public init(text: String, isRepeat: Bool = false, isCorrect: Bool = true, isBest: Bool = false) {
         self.text = text
         self.isRepeat = isRepeat
         self.isCorrect = isCorrect
